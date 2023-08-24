c0ced291dc61f4df370dba95cf7e8db4535b7c96
Artemzaicew <122361190+Artemzaicew@users.noreply.github.com>
2023-08-24 22:14:58 +0300

    add first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,68 @@
-import Feedback from "./Feedback/Feedback";
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-     <Feedback/>
-    </div>
-  );
-};
+// import Feedback from "./Feedback/Feedback";
+import { Component } from "react";
+import Statistics from "./Statistics/Statistics";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions"
+
+class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  goodIncrement = () => {
+    this.setState({
+      good: this.state.good + 1
+    })
+  }
+
+  neutralIncrement = () => {
+    this.setState({
+      neutral: this.state.neutral + 1
+    })
+  }
+
+  badIncrement = () => {
+    this.setState({
+      bad: this.state.bad + 1
+    })
+  }
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad
+  };
+
+  countPositiveFeedbackPercentage = () =>
+    ((this.state.good / this.countTotalFeedback()) * 100).toFixed(2);
+
+  render() {
+    return (
+      <div className="">
+        <p>Please leave feedback</p>
+
+        <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleFeedback}
+          />
+
+        <Statistics
+          good = {this.state.good}
+          neutral = {this.state.neutral}
+          bad = {this.state.bad}
+          total = {this.countTotalFeedback()}
+          positivePercentage = {this.countPositiveFeedbackPercentage()}
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
